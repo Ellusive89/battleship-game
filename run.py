69472d2e39b2dcfabebdc5a8fedd94463adb8885
Ellusive89 <eva.nagrodzka@gmail.com>
2023-04-23 03:35:31 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+BOARD_SIZE = 8
+SIZE_OF_SHIPS = [1, 2, 3, 4, 5, 6]
+LETT_TO_NUM = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+SHIP_SYMBOLS = {1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6'}
+
+
+def main():
+
+    print("Welcome to Battleship Game!")
