73e20692c104eb37d0a945c50e5388e2254ef745
Ellusive89 <eva.nagrodzka@gmail.com>
2023-04-23 03:49:44 +0000

    Add welcome message and function creating empty board

--- run.py
+++ run.py
@@ -1,11 +1,19 @@
 import random
 
 BOARD_SIZE = 8
-SIZE_OF_SHIPS = [1, 2, 3, 4, 5, 6]
+SHIP_SIZE = [1, 2, 3, 4, 5, 6]
 LETT_TO_NUM = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 SHIP_SYMBOLS = {1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6'}
 
 
+def create_empty_board():
+    return [[" "] * BOARD_SIZE for _ in range(BOARD_SIZE)]
+
+
 def main():
 
-    print("Welcome to Battleship Game!")
+    print("=== Welcome to Battleship Game! ===")
+    create_empty_board()
+
+
+main()
