6891cc2f527e102c3511041214585cda744e18b8
Ellusive89 <eva.nagrodzka@gmail.com>
2023-04-23 15:22:08 +0000

    Add function asking for players name and function printing game board

--- run.py
+++ run.py
@@ -10,10 +10,31 @@ def create_empty_board():
     return [[" "] * BOARD_SIZE for _ in range(BOARD_SIZE)]
 
 
+def get_player_name():
+    while True:
+        player_name = input("Enter your name: ").upper()
+        if player_name.isalpha():
+            return player_name
+        else:
+            print("Please enter a name containing only letters.")
+
+
+def print_board(board, player_name):
+    """Prints the game board with the player's name."""
+    print(f"\n{player_name}'s Board")
+    print("  A B C D E F G H")
+    for i in range(BOARD_SIZE):
+        row = [str(i + 1)]
+        for j in range(BOARD_SIZE):
+            row.append(board[i][j])
+        print(" ".join(row))
+
+
 def main():
 
     print("=== Welcome to Battleship Game! ===")
     create_empty_board()
+    player_name = get_player_name()
 
 
 main()
